c757d64acf7f01b9fd813f291b454051bb1c5cb3
Mikhail <mishadnepr347@gmail.com>
2021-03-23 11:12:05 +0200

    Added indexator and constructor

--- MyLinkedList/LinkedListClass.cs
+++ MyLinkedList/LinkedListClass.cs
@@ -5,30 +5,100 @@ namespace MyLinkedList
     public class LinkedListClass<T> : IList<T> where T : IComparable
     {
         public Node<T> Head { get; set; }
-        
+
         public Node<T> Tail { get; set; }
 
-        public int Count { get; set; }
+        public int Length { get; set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (IsValidIndex(index))
+                {
+                    Node<T> current = Head;
+
+                    for (int i = 1; i <= index; i++)
+                    {
+                        current = current.Next;
+                    }
+
+                    return current.Data;
+                }
+
+                throw new IndexOutOfRangeException("Invalid index!");
+            }
+            set
+            {
+                if (IsValidIndex(index) && !(value is null))
+                {
+                    Node<T> current = Head;
+
+                    for (int i = 1; i <= index; i++)
+                    {
+                        current = current.Next;
+                    }
+
+                    current.Data = value;
+                }
+                else if (!(IsValidIndex(index)))
+                {
+                    throw new IndexOutOfRangeException("Invalid index");
+                }
+                else
+                {
+                    throw new NullReferenceException("Data cannot be null");
+                }
+            }
+        }
 
         public LinkedListClass()
         {
+            Length = 0;
             Head = null;
             Tail = null;
-            Count = 0;
         }
 
         public LinkedListClass(T data)
         {
-            if(data!= null)
+            if (data != null)
             {
-                Node<T> item = new Node<T>(data);
-                Head = item;
-                Tail = item;
-                ++Count;
+                Length = 1;
+                Head = new Node<T>(data);
+                Tail = Head;
             }
             else
             {
                 throw new ArgumentNullException("Null data passed");
+            }
+        }
+
+        public LinkedListClass(T[] data)
+        {
+            if (!(data is null))
+            {
+                Length = data.Length;
+
+                if (data.Length != 0)
+                {
+                    Head = new Node<T>(data[0]);
+                    Tail = Head;
+
+                    for (int i = 1; i < data.Length; i++)
+                    {
+                        Tail.Next = new Node<T>(data[i]);
+                        Tail = Tail.Next;
+                    }
+                }
+                else
+                {
+                    Head = null;
+                    Tail = null;
+                }
+            }
+            else
+            {
+                throw new NullReferenceException("Null data passed!");
             }
         }
 
@@ -150,6 +220,11 @@ namespace MyLinkedList
         public void HalfReverse()
         {
             throw new NotImplementedException();
+        }
+
+        private bool IsValidIndex(int index)
+        {
+            return index >= 0 && index < Length;
         }
     }
 }
