9a9419f490c4d700eab1cee939411837bf36a3d3
Mikhail <mishadnepr347@gmail.com>
2021-03-23 10:49:41 +0200

    Implemented methods

--- MyLinkedList/LinkedListClass.cs
+++ MyLinkedList/LinkedListClass.cs
@@ -2,7 +2,7 @@
 
 namespace MyLinkedList
 {
-    public class LinkedListClass<T>
+    public class LinkedListClass<T> : IList<T> where T : IComparable
     {
         public Node<T> Head { get; set; }
         
@@ -30,6 +30,126 @@ namespace MyLinkedList
             {
                 throw new ArgumentNullException("Null data passed");
             }
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T[] ToArray()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddStart(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddByIndex(int index, T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRangeStart(T[] collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(T[] collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRangeByIndex(int index, T[] collection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveByIndex(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T RemoveStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRangeByIndex(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRangeStart()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveByValueFirst(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int RemoveByValueAll(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FindIndexByValue(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetMaxIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetMax()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetMinIndex()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sort(bool isAscending)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reverse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HalfReverse()
+        {
+            throw new NotImplementedException();
         }
     }
 }
